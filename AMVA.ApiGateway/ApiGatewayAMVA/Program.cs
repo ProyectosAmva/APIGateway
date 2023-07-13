5fe5a61b3d53e37a0d41830da47b1206825a14a8
Jorge Enrique Estrada Correa <Proyectosamva@gmail.com>
2023-07-13 07:07:55 -0500

    Ajuste en DTO Tareas

--- AMVA.ApiGateway/ApiGatewayAMVA/Program.cs
+++ AMVA.ApiGateway/ApiGatewayAMVA/Program.cs
@@ -1,17 +1,27 @@
+using AMVA.JwtAuthenticationManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using AMVA.JwtAuthenticationManager;
-
+using Ocelot.Tracing.Butterfly;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllers();
 
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddOcelot();
+
+
+builder.Services.AddOcelot().AddButterfly(option =>
+{
+    //this is the url that the butterfly collector server is running on...
+    option.CollectorUrl = "http://localhost:9618";
+    option.Service = "Ocelot";
+});
+
+
+
 builder.Services.AddCustomJwtAuthenticationApp();
 
 
@@ -26,8 +36,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 await app.UseOcelot();
