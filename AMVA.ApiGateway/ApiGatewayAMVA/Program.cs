6dcdad6f733a3f911023fe88fc6c0c8a58976558
Reinaldo Barco Lopez <reyblop@gmail.com>
2024-04-18 22:12:29 -0500

    Abril 18 2024 Se le quita ButterFly

--- AMVA.ApiGateway/ApiGatewayAMVA/Program.cs
+++ AMVA.ApiGateway/ApiGatewayAMVA/Program.cs
@@ -1,7 +1,6 @@
 using AMVA.JwtAuthenticationManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Tracing.Butterfly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
-builder.Services.AddOcelot().AddButterfly(option =>
-{
-    //this is the url that the butterfly collector server is running on...
-    option.CollectorUrl = "http://localhost:9618";
-    option.Service = "Ocelot";
-});
+builder.Services.AddOcelot();
 
 
 
